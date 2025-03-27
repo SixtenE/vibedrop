3e8d15622f4cdbf415a9837d741ef64aeb8d5f6c
SixtenE <sixten.ekblad@gmail.com>
2025-03-27 13:24:53 +0100

    supabase får inte att funka

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import "./App.css";
 import { useDropzone } from "react-dropzone";
+import { createClient } from "@supabase/supabase-js";
 
-function App() {
+// Create Supabase client
+const supabase = createClient(
+  "https://nbodsrunndqzztsvilcc.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ib2RzcnVubmRxenp0c3ZpbGNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTgyMDUsImV4cCI6MjA1MzE5NDIwNX0.mYnhoewTWWhA47PtEMXiqW9bx6zTkQN19GKCoQjGzk8"
+);
+
+// Upload file using standard upload
+async function uploadFile(file: File) {
+  const { data, error } = await supabase.storage
+    .from("vibe")
+    .upload("charles", file);
+  if (error) {
+    // Handle error
+  } else {
+    // Handle success
+  }
+  console.log(data);
+}
+
+export default function App() {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
 
   const files = acceptedFiles.map((file) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
+    <li key={file.path}>{`${file.path} - ${file.size} bytes`}</li>
   ));
 
   return (
@@ -20,8 +38,14 @@ function App() {
         <h4>Files</h4>
         <ul>{files}</ul>
       </aside>
+      <button
+        disabled={acceptedFiles.length === 0}
+        onClick={async () => {
+          uploadFile(acceptedFiles[0]);
+        }}
+      >
+        Upload
+      </button>
     </section>
   );
 }
-
-export default App;
